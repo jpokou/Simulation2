07dc7cc6d21725b36d1f8f4af1ffd0e391330df8
jpokou <j.pokou@hotmail.com>
2013-04-05 22:46:24 -0400

    Initalisation du Projet du Samedi 06 Avril 2013: Le projet "PJ-TournoiRunes" vise à développer un logiciel de simulation de tournoi de runes. Afin de bien répondre aux exigences du client, plusieurs fonctionnalités du logiciel sont à considérer. Il s’agit des règles du jeu, de l'affichage des résultats,  de l'interface graphique et le déroulement du jeu.
    Votre mission de projet de développement
    
    Vous devez développer un logiciel en Java (contrainte) qui simule le tournoi de runes des druides de la forêt des Carnutes. Le client a fourni les grandes lignes (sous forme d'épisodes ou epics) de ses priorités pour ce logiciel:
    (Essentiel) Visualiser à la console les résultats d'un tour pour chacun des groupes, sous forme texte. Les groupes et les druides sont alors codés en dur (hard coded).
    (Essentiel) Implémenter toutes les règles du tournoi, sauf la dernière (celle de l'atteinte des 20 Aureüs).
    (Essentiel) Enregistrer un tournoi en cours et pouvoir continuer ce tournoi au prochain démarrage du jeu.
    (Essentiel) Simuler un nouveau tournoi du début à la fin à la console.
    (Important) Conserver des statistiques par druide: nombre de participations à un tournoi, en demi-finales et en finales; Nombre de fois où il a été désactivé (gains cumulés sous zéro); Nombre de runes d'or cumulées à vie. Ces statistiques sont conservées par année (équivalent à être conservées par tournoi).
    (Important) Être capable d'éditer manuellement les statistiques d'un druide, pour n'importe quel tournoi passé.
    (Important) Implémenter la dernière règle (celle de l'atteinte des 20 Aureüs).
    (Important) Simuler un nouveau tournoi du début à la fin avec une interface graphique. Il y aurait alors un bouton ou un lien pour démarrer le tournoi et un autre pour démarrer un tour. 
    (Souhaitable) Être capable de gérer les druides d'un groupe: ajouter, supprimer, renommer, et remplacer (déplacement d'un druide d'un groupe à un autre).
    (Souhaitable) Être capable de gérer des groupes: créer, renommer, supprimer (inactiver, car les statistiques du groupe demeurent).
    (Souhaitable) Visualiser graphiquement le déroulement du tournoi.

--- src/Test/Test.java
+++ src/Test/Test.java
@@ -2,18 +2,19 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package Test;
+
+//package Test;
 
 /**
  *
  * @author Boris
  */
-public class Test {
+//public class Test {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    //public static void main(String[] args) {
         // TODO code application logic here
-    }
-}
+    //}
+//}
